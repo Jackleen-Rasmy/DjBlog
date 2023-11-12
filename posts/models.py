d7c5a78c12ee44be2e7d6d843485dde82e64237f
Jackleen Rasmy <jackleen.rasmy@gmail.com>
2023-11-12 03:28:18 +0200

    models-ERD

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
-# Create your models here.
+'''
+-title
+-content
+-draft
+-publish date
+-author
+-image
+-tags
+-category
+-comments
+'''
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=20000)
+    draft = models.BooleanField(default=True)
+    publish_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, related_name='author_post', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post',blank=True,null=True)
+    tags = TaggableManager()
+    category = models.ForeignKey('Category', related_name='post_category', on_delete=models.SET_NULL,null=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, related_name='post_comment', on_delete=models.CASCADE)
+    user = models.CharField(max_length=50)
+    comment = models.TextField(max_length=300)
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return str(self.post)
+
+    
+    
+    
+    
+    
+    
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+    'taggit',
+    'django_extensions',
+    'pydot',
+
+
 ]
 
 MIDDLEWARE = [
@@ -117,7 +123,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPH_MODELS = {
+  'all_applications': False,
+  'group_models': True,
+}
